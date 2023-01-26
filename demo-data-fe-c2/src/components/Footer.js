39ec88d9c11d946aed4967225ac5638e54c23711
Romil Shah <romilshah2199@gmail.com>
2023-01-26 17:25:58 +0530

    added icons

--- demo-data-fe-c2/src/components/Footer.js
+++ demo-data-fe-c2/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+  FaGithubSquare,
+  FaDribbbleSquare,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="px-4 py-12 bg-black">
-      <div className="max-w-[1240px] mx-auto grid grid-cols-1 gap-y-2 md:grid-cols-4 md:gap-y-0 md:gap-x-8">
+      <div className="max-w-[1240px] mx-auto grid grid-cols-1 gap-y-6 md:grid-cols-4 md:gap-y-0 md:gap-x-8">
         <div className="flex flex-col space-y-4 md:col-span-1">
           <h3 className="uppercase text-brightGreen text-lg font-bold md:text-xl">
             react.
@@ -13,10 +20,17 @@ const Footer = () => {
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Interdum
             velit laoreet id donec
           </p>
+          <div className="grid grid-cols-5 gap-x-2 text-white">
+            <FaFacebookSquare className="cursor-pointer text-2xl  md:text-lg" />
+            <FaInstagramSquare className="cursor-pointer text-2xl  md:text-lg" />
+            <FaTwitterSquare className="cursor-pointer text-2xl  md:text-lg" />
+            <FaGithubSquare className="cursor-pointer text-2xl  md:text-lg" />
+            <FaDribbbleSquare className="cursor-pointer  text-2xl md:text-lg" />
+          </div>
         </div>
 
         {/* links container */}
-        <div className="grid grid-cols-1 gap-y-2 md:grid-cols-3 md:col-span-3 md:gap-y-0 md:gap-x-2 text-white">
+        <div className="grid grid-cols-3 gap-x-2 text-white md:col-span-3">
           <div className="flex flex-col space-y-3">
             <a href="/">Solutions</a>
             <a href="/">Analytics</a>
