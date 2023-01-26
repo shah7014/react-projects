fbc1855602cece8e311c10cb5cdfee0d1a38bb4f
Romil Shah <romilshah2199@gmail.com>
2023-01-26 13:37:20 +0530

    nav bar added

---
+++ demo-data-fe-c2/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Hero, NavBar, Introduction } from "./components";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Introduction />
+    </div>
+  );
+}
+
+export default App;
