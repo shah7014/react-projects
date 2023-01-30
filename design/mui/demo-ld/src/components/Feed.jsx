9ce55c4811464d80aaede1ee824e63f4cbd05f03
Romil Shah <romilshah2199@gmail.com>
2023-01-30 22:17:34 +0530

    added more mui comp

--- design/mui/demo-ld/src/components/Feed.jsx
+++ design/mui/demo-ld/src/components/Feed.jsx
@@ -1,8 +1,16 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Post from "./Post";
 
 const Feed = () => {
-  return <Box sx={{ backgroundColor: "red", p: 2, flex: 4 }}>Feed</Box>;
+  return (
+    <Box sx={{ p: 2, flex: 4 }}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Box>
+  );
 };
 
 export default Feed;
